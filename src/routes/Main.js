1b29883e01181b03386b5275e2769a099d7ff597
Nath Mendoza <mendoza.nathaniel328@gmail.com>
2021-02-25 19:23:02 +0800

    test for heroku

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -71,7 +71,7 @@ const Main = ({tch}) => {
                         <div className="hero-content-wrap">
                             <div className="pic-wrap"><img src={process.env.PUBLIC_URL + 'images/pic-of-me.jpg'} alt="test"/></div>
                             <h2>{greetMessage()}</h2>
-                            <h6>I'm Nath, A front-end web developer</h6>
+                            <h6>I'm Nath, A front-enddddd web developer</h6>
                             <p>Heya, I see you're visiting my portfolio. Do check around this page to see the summary of who I am. Also, don't forget to check 
                                 out my <Link to="/projects">Projects</Link> page to see what I've accomplished through the length of my field.</p>
                         </div>
